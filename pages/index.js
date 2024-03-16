ec70fef097db775865d6a6006cc3f0f45d8b5d43
Wesley Gessner <113656364+wesleygessner@users.noreply.github.com>
2024-03-16 04:18:21 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home(){
+    return(
+        <div>
+            <h1>Seja você mesma sempre!</h1>
+            <p>
+                É óbvio que você irá magoar algumas pessoas no caminho, na verdade, até você mesma irá se machucar. Porém o que lhe convém é aprender com os seus erros e se tornar uma pessoa melhor.
+            </p>
+        </div>
+    )
+}
+
+export default Home;
